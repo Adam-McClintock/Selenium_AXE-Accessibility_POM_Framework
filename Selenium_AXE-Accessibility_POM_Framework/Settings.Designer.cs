4fe4bf6694d6db716f1589220ec462b454443e28
Adam-McClintock <adammcclintock@live.co.uk>
2023-02-20 16:01:52 +0000

    Update settings file to headless

--- Selenium_AXE-Accessibility_POM_Framework/Settings.Designer.cs
+++ Selenium_AXE-Accessibility_POM_Framework/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace Selenium_AXE_Accessibility_POM_Framework {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool Headless {
             get {
                 return ((bool)(this["Headless"]));
